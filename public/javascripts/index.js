fb54d29064ead86e0a152cb9730cfabd5cbb567b
Natalie Romano <brandnew03@gmail.com>
2015-08-06 12:22:46 -0700

    Initial commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by natalie on 8/5/2015.
+ */
+
+    console.log('Hello from public/');
+
+/*
+ Create an Express application that takes the email and password for a gmail account and shows
+
+ on a dashboard, the number of emails received by the user as three pie charts
+
+ 1. Read vs Unread emails
+
+ 2. Emails with attachments vs emails without attachments
+
+ 3. Division based on MIME types of attachments (doc, pdf, etc) - This should be dynamic
+
+ based on the MIME types DO NOT hardcode mime types
+ */
---
+++ routes/index.js
@@ -0,0 +1,179 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+var Imap = require('imap'),
+    inspect = require('util').inspect;
+
+var imap = new Imap({
+  user: 'EMAIL-ADDRESS',
+  password: 'YOUR-PASSWORD',
+  host: 'imap.gmail.com',
+  port: 993,
+  tls: true
+});
+
+var fs = require('fs'), fileStream;
+
+function openInbox(cb) {
+  imap.openBox('INBOX', true, cb);
+}
+
+var unreadCount = 0;
+var readCount = 0;
+var attachCount = 0;
+var totalEmails = 0;
+var mimes_arr = [];
+
+imap.once('ready', function() {
+    openInbox(function(err, box) {
+        if (err) throw err;
+        imap.search([ 'UNSEEN', ['SINCE', 'July 20, 2015'] ], function(err, results) {
+            if (err) throw err;
+            var f = imap.fetch(results, { bodies: '' });
+            f.on('message', function(msg, seqno) {
+                var prefix = '(#' + seqno + ') ';
+                msg.on('body', function(stream, info) {
+                    unreadCount++;
+                });
+                msg.once('attributes', function(attrs) {
+                });
+                msg.once('end', function() {
+                    console.log(prefix + 'Finished');
+                });
+            });
+            f.once('error', function(err) {
+                console.log('Fetch error: ' + err);
+            });
+            f.once('end', function() {
+                imap.end();
+            });
+        });
+    });
+});
+
+imap.once('ready', function() {
+    openInbox(function (err, box) {
+        if (err) throw err;
+        imap.search([ 'SEEN', ['SINCE', 'July 20, 2015'] ], function(err, results) {
+            if (err) throw err;
+            var f = imap.fetch(results, { bodies: '' });
+            f.on('message', function(msg, seqno) {
+                var prefix = '(#' + seqno + ') ';
+                msg.on('body', function(stream, info) {
+                    readCount++;
+                });
+                msg.once('attributes', function(attrs) {
+                });
+                msg.once('end', function() {
+                    console.log(prefix + 'Finished');
+                });
+            });
+            f.once('error', function(err) {
+                console.log('Fetch error: ' + err);
+            });
+            f.once('end', function() {
+                imap.end();
+            });
+        });
+    });
+});
+
+imap.once('ready', function() {
+  openInbox(function(err, box) {
+    if (err) throw err;
+    imap.search([ 'ALL', ['SINCE', 'July 20, 2015'] ], function(err, results) {
+      if (err) throw err;
+      var f = imap.fetch(results, { bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)', struct: true });
+        f.on('message', function(msg, seqno) {
+            var prefix = '(#' + seqno + ') ';
+            msg.on('body', function(stream, info) {
+                var buffer = '';
+                stream.on('data', function(chunk) {
+                    buffer += chunk.toString('utf8');
+                });
+                stream.once('end', function() {
+                });
+            });
+            msg.once('attributes', function(attrs) {
+                totalEmails++;
+                var arr = attrs.struct[2];
+                if (typeof arr !== 'undefined'){
+                    if(arr[0].disposition === null || typeof arr[0].disposition === 'undefined' || typeof arr[0].disposition.params === 'undefined'){
+                    }
+                    else{
+                        attachCount++;
+
+                        var thisType = arr[0].type;
+                        var found = false;
+
+                        if (mimes_arr.length === 0){
+                            var obj = {mtype: thisType, count: 1};
+                            mimes_arr.push(obj);
+                        }
+                        else{
+                            mimes_arr.forEach(function (mime) {
+                                if (mime.mtype === thisType){
+                                    found = true;
+                                    mime.count++;
+                                }
+                            });
+                            if (found === false){
+                                var obj = {mtype: thisType, count: 1};
+                                mimes_arr.push(obj);
+                            }
+                            found = false;
+                        }
+
+                    }
+                }
+
+                console.log('LOOK HERE', mimes_arr);
+            });
+            msg.once('end', function() {
+                console.log(prefix + 'Finished');
+            });
+        });
+        f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+            console.log('Done fetching all messages!');
+            imap.end();
+      });
+    });
+  });
+});
+
+imap.once('error', function(err) {
+  console.log(err);
+});
+
+imap.once('end', function() {
+    var nonAttachments = totalEmails - attachCount;
+  console.log('Connection ended');
+});
+
+imap.connect();
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {var nonAttachments = totalEmails - attachCount;
+    var renderObj = {
+        title: 'Express',
+        unread: unreadCount,
+        read: readCount,
+        attachments: attachCount,
+        total: totalEmails,
+        noatt: nonAttachments,
+        numTypes: mimes_arr.length,
+        mimes_arr: mimes_arr
+    }
+    console.log("my object",renderObj);
+  res.render('index', renderObj);
+});
+
+module.exports = router;
