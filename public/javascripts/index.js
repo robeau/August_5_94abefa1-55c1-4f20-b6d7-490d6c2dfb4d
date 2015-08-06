73f8df5385c314a7e20278cc6e51175ea39c24ea
Natalie Romano <brandnew03@gmail.com>
2015-08-06 14:53:56 -0700

    updated css

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,3 +17,63 @@
 
  based on the MIME types DO NOT hardcode mime types
  */
+
+var calendar = document.getElementById('date');
+var shownDate = document.getElementById('showDate');
+
+calendar.addEventListener('change', function () {
+ var newDate = this.value;
+ newDate = newDate.toString();
+ var year = newDate.substr(0,4);
+ var month = newDate.substr(5,2);
+ switch (month){
+  case '01':
+         month = 'January';
+         break;
+  case '02':
+   month = 'February';
+   break;
+  case '03':
+   month = 'March';
+   break;
+  case '04':
+   month = 'April';
+   break;
+  case '05':
+   month = 'May';
+   break;
+  case '06':
+   month = 'June';
+   break;
+  case '07':
+   month = 'July';
+   break;
+  case '08':
+   month = 'August';
+   break;
+  case '09':
+   month = 'September';
+   break;
+  case '10':
+   month = 'October';
+   break;
+  case '11':
+   month = 'November';
+   break;
+  case '12':
+   month = 'December';
+   break;
+ }
+ var day = newDate.substr(8,2);
+ newDate = month + ' ' + day + ', ' + year;
+
+ console.log('new date is:', newDate);
+
+ $.post('/', newDate, function (data) {
+  console.log('this is the data',data);
+ });
+
+ shownDate.innerHTML = 'since ' + newDate;
+ location.reload();
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('./config');
 var Imap = require('imap'),
     inspect = require('util').inspect;
 
-var imap = new Imap({
-  user: 'EMAIL@gmail.com',
-  password: 'PASSWORD',
-  host: 'imap.gmail.com',
-  port: 993,
-  tls: true
-});
+var imap = new Imap(config);
 
 var fs = require('fs'), fileStream;
 
@@ -24,149 +19,161 @@ var attachCount = 0;
 var totalEmails = 0;
 var mimes_arr = [];
 
-imap.once('ready', function() {
-    openInbox(function(err, box) {
-        if (err) throw err;
-        imap.search([ 'UNSEEN', ['SINCE', BACKDATE] ], function(err, results) {
+function parseEmails (){
+    imap.once('ready', function() {
+        openInbox(function(err, box) {
             if (err) throw err;
-            var f = imap.fetch(results, { bodies: '' });
-            f.on('message', function(msg, seqno) {
-                var prefix = '(#' + seqno + ') ';
-                msg.on('body', function(stream, info) {
-                    unreadCount++;
+            imap.search([ 'UNSEEN', ['SINCE', BACKDATE] ], function(err, results) {
+                if (err) throw err;
+                var f = imap.fetch(results, { bodies: '' });
+                f.on('message', function(msg, seqno) {
+                    var prefix = '(#' + seqno + ') ';
+                    msg.on('body', function(stream, info) {
+                        unreadCount++;
+                    });
+                    msg.once('attributes', function(attrs) {
+                    });
+                    msg.once('end', function() {
+                        console.log(prefix + 'Finished');
+                    });
                 });
-                msg.once('attributes', function(attrs) {
+                f.once('error', function(err) {
+                    console.log('Fetch error: ' + err);
                 });
-                msg.once('end', function() {
-                    console.log(prefix + 'Finished');
+                f.once('end', function() {
+                    imap.end();
                 });
-            });
-            f.once('error', function(err) {
-                console.log('Fetch error: ' + err);
-            });
-            f.once('end', function() {
-                imap.end();
             });
         });
     });
-});
 
-imap.once('ready', function() {
-    openInbox(function (err, box) {
-        if (err) throw err;
-        imap.search([ 'SEEN', ['SINCE', BACKDATE] ], function(err, results) {
+    imap.once('ready', function() {
+        openInbox(function (err, box) {
             if (err) throw err;
-            var f = imap.fetch(results, { bodies: '' });
-            f.on('message', function(msg, seqno) {
-                var prefix = '(#' + seqno + ') ';
-                msg.on('body', function(stream, info) {
-                    readCount++;
+            imap.search([ 'SEEN', ['SINCE', BACKDATE] ], function(err, results) {
+                if (err) throw err;
+                var f = imap.fetch(results, { bodies: '' });
+                f.on('message', function(msg, seqno) {
+                    var prefix = '(#' + seqno + ') ';
+                    msg.on('body', function(stream, info) {
+                        readCount++;
+                    });
+                    msg.once('attributes', function(attrs) {
+                    });
+                    msg.once('end', function() {
+                        console.log(prefix + 'Finished');
+                    });
                 });
-                msg.once('attributes', function(attrs) {
+                f.once('error', function(err) {
+                    console.log('Fetch error: ' + err);
                 });
-                msg.once('end', function() {
-                    console.log(prefix + 'Finished');
+                f.once('end', function() {
+                    imap.end();
                 });
-            });
-            f.once('error', function(err) {
-                console.log('Fetch error: ' + err);
-            });
-            f.once('end', function() {
-                imap.end();
             });
         });
     });
-});
 
-imap.once('ready', function() {
-  openInbox(function(err, box) {
-    if (err) throw err;
-    imap.search([ 'ALL', ['SINCE', BACKDATE] ], function(err, results) {
-      if (err) throw err;
-      var f = imap.fetch(results, { bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)', struct: true });
-        f.on('message', function(msg, seqno) {
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function(stream, info) {
-                var buffer = '';
-                stream.on('data', function(chunk) {
-                    buffer += chunk.toString('utf8');
-                });
-                stream.once('end', function() {
-                });
-            });
-            msg.once('attributes', function(attrs) {
-                totalEmails++;
-                var arr = attrs.struct[2];
-                if (typeof arr !== 'undefined'){
-                    if(arr[0].disposition === null || typeof arr[0].disposition === 'undefined' || typeof arr[0].disposition.params === 'undefined'){
-                    }
-                    else{
-                        attachCount++;
-
-                        var thisType = arr[0].type;
-                        var found = false;
-
-                        if (mimes_arr.length === 0){
-                            var obj = {mtype: thisType, count: 1};
-                            mimes_arr.push(obj);
-                        }
-                        else{
-                            mimes_arr.forEach(function (mime) {
-                                if (mime.mtype === thisType){
-                                    found = true;
-                                    mime.count++;
-                                }
-                            });
-                            if (found === false){
-                                var obj = {mtype: thisType, count: 1};
-                                mimes_arr.push(obj);
+    imap.once('ready', function() {
+        openInbox(function(err, box) {
+            if (err) throw err;
+            imap.search([ 'ALL', ['SINCE', BACKDATE] ], function(err, results) {
+                if (err) throw err;
+                var f = imap.fetch(results, { bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)', struct: true });
+                f.on('message', function(msg, seqno) {
+                    var prefix = '(#' + seqno + ') ';
+                    msg.on('body', function(stream, info) {
+                        var buffer = '';
+                        stream.on('data', function(chunk) {
+                            buffer += chunk.toString('utf8');
+                        });
+                        stream.once('end', function() {
+                        });
+                    });
+                    msg.once('attributes', function(attrs) {
+                        totalEmails++;
+                        var arr = attrs.struct[2];
+                        if (typeof arr !== 'undefined'){
+                            if(arr[0].disposition === null || typeof arr[0].disposition === 'undefined' || typeof arr[0].disposition.params === 'undefined'){
                             }
-                            found = false;
+                            else{
+                                attachCount++;
+
+                                var thisType = arr[0].type;
+                                var found = false;
+
+                                if (mimes_arr.length === 0){
+                                    var obj = {mtype: thisType, count: 1};
+                                    mimes_arr.push(obj);
+                                }
+                                else{
+                                    mimes_arr.forEach(function (mime) {
+                                        if (mime.mtype === thisType){
+                                            found = true;
+                                            mime.count++;
+                                        }
+                                    });
+                                    if (found === false){
+                                        var obj = {mtype: thisType, count: 1};
+                                        mimes_arr.push(obj);
+                                    }
+                                    found = false;
+                                }
+
+                            }
                         }
-
-                    }
-                }
-            });
-            msg.once('end', function() {
-                console.log(prefix + 'Finished');
+                    });
+                    msg.once('end', function() {
+                        console.log(prefix + 'Finished');
+                    });
+                });
+                f.once('error', function(err) {
+                    console.log('Fetch error: ' + err);
+                });
+                f.once('end', function() {
+                    console.log('Done fetching all messages!');
+                    imap.end();
+                });
             });
         });
-        f.once('error', function(err) {
-            console.log('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-            console.log('Done fetching all messages!');
-            imap.end();
-      });
     });
-  });
-});
 
-imap.once('error', function(err) {
-  console.log(err);
-});
+    imap.once('error', function(err) {
+        console.log(err);
+    });
 
-imap.once('end', function() {
-    var nonAttachments = totalEmails - attachCount;
-  console.log('Connection ended');
-});
+    imap.once('end', function() {
+        var nonAttachments = totalEmails - attachCount;
+        console.log('Connection ended');
+    });
 
-imap.connect();
+    imap.connect();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {var nonAttachments = totalEmails - attachCount;
-    var renderObj = {
-        title: 'Express',
-        unread: unreadCount,
-        read: readCount,
-        attachments: attachCount,
-        total: totalEmails,
-        noatt: nonAttachments,
-        numTypes: mimes_arr.length,
-        mimes_arr: mimes_arr,
-        date: BACKDATE
-    }
-  res.render('index', renderObj);
-});
+    /* GET home page. */
+    router.get('/', function(req, res, next) {var nonAttachments = totalEmails - attachCount;
+        var renderObj = {
+            title: 'Express',
+            unread: unreadCount,
+            read: readCount,
+            attachments: attachCount,
+            total: totalEmails,
+            noatt: nonAttachments,
+            numTypes: mimes_arr.length,
+            mimes_arr: mimes_arr,
+            date: BACKDATE
+        }
+        res.render('index', renderObj);
+    });
 
-module.exports = router;
+    module.exports = router;
+}
+
+parseEmails();
+
+router.post('/', function (req,res) {
+    BACKDATE = Object.getOwnPropertyNames(req.body)[0];
+    console.log('BACKDATE', Object.getOwnPropertyNames(req.body));
+    parseEmails();
+    res.status(200).json({message: 'Successfully changed the date.'});
+})
+
